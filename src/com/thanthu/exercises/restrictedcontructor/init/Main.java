ec0f82535f0c9501b00458fb814169b28a46f69c
Thanthu <tnair@orthofx.com>
2021-09-05 13:45:06 +0530

    exercise - restricted constructor access

---
+++ src/com/thanthu/exercises/restrictedcontructor/init/Main.java
@@ -0,0 +1,27 @@
+package com.thanthu.exercises.restrictedcontructor.init;
+
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import com.thanthu.exercises.restrictedcontructor.web.WebServer;
+
+public class Main {
+
+	public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException,
+			InstantiationException, IllegalAccessException, IOException {
+		initConfiguration();
+		WebServer webServer = new WebServer();
+		webServer.startServer();
+	}
+
+	public static void initConfiguration()
+			throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+		Constructor<ServerConfiguration> constructor = ServerConfiguration.class.getDeclaredConstructor(int.class,
+				String.class);
+
+		constructor.setAccessible(true);
+		constructor.newInstance(8080, "Good Day!");
+	}
+
+}
